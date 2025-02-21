36715ded00f4b3f150ad142fdb22b3ea80edcbbb
Bhumika Sharma <bhumii85sharma@gmail.com>
2025-02-21 22:57:51 +0530

    add Exceptional Handling File

---
+++ ExceptionHandling/Program8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program8
+    {
+        static void Method1()
+        {
+            int v = 10 / 0;
+            int result = v; // This will throw an ArithmeticException
+        }
+
+        static void Method2()
+        {
+            Method1(); // Exception propagates from here
+        }
+
+        static void Main()
+        {
+            try
+            {
+                Method2(); // Calls Method2(), which calls Method1()
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Handled exception in Main");
+            }
+        }
+    }
+
+}
